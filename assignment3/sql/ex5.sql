f0c0c1084f96f00ec382fb0c4541684d619bb60c
timofeyh <timofeyhartanovich@gmail.com>
2021-11-26 10:31:24 -0500

    added last select statement

--- assignment3/sql/ex5.sql
+++ assignment3/sql/ex5.sql
@@ -61,3 +61,22 @@ where exists(select * from supply_orders s
 		join Bill_Of_Materials b on s.Material = b.Finished_Good
 		inner join Materials m2 on b.Child = m2.ID
 		where m2.Stock < b.Qty * s.Qty and s.Material = m.ID and s.Status <> "COMPLETE" and s.Status <> "PRODUCTION"); 
+
+-- Return the total amount of raw materials that would cover all current demand orders
+with recursive receipt(n, FG, FG_txt, Parent, Parent_txt, Child, Child_txt, Qty, UOM, Scrap, Price,`Group`) as 
+(
+	select 1 n, m.ID, m.Item_Description, m.ID, m.Item_Description, m2.ID Child, m2.Item_Description, CAST(1.00 as double) QTY, m2.UOM, 0.00 Scrap, m2.Price Price, m2.Group_ID From bill_of_materials b
+    inner join Materials m on b.Finished_Good = m.ID
+    inner join Materials m2 on b.Child = m2.ID
+    Union All
+    select 1 + n, r.FG, r.FG_Txt, m.ID, m.Item_Description, m2.ID, m2.Item_Description, b.Qty*r.Qty, m2.UOM, b.Scrap, m2.Price, m2.Group_ID From Bill_Of_Materials b
+    inner join Materials m on b.Finished_Good = m.ID
+    inner join Materials m2 on b.Child = m2.ID
+    inner join receipt r on r.Child = m.ID
+)
+select MAX(m2.Item_Description) Raw_Material, Sum(r.Qty*d.Qty) Qty_Ordered, MAX(m2.UOM) UOM from demand_orders d 
+inner join materials m on m.ID = d.Material 
+left join receipt r on r.FG = m.ID 
+inner join materials m2 on r.Child = m2.ID 
+where m2.Group_ID = "RM"
+group by m2.Item_Description;
