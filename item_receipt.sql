1f9903da21817524f27e5d34278d60e6a8f9b804
AndrewLou2 <louandrew123@gmail.com>
2021-11-25 21:14:37 -0500

    Update item_receipt.sql

--- item_receipt.sql
+++ item_receipt.sql
@@ -1,12 +1,10 @@
 use databasics;
-SET @QTY=CAST(50 as double);
-with recursive receipt(n, ID, Item_Description, Qty, UOM, Scrap, Price) as 
+with recursive receipt(n, FG, ID, Item_Description, Qty, UOM, Scrap, Price) as 
 (
-	select 1 n, ID ID, Item_Description Item_Desc, @Qty QTY, UOM, 0.00 Scrap, @Qty*Price Price From Materials
-    where ID = 1840
+	select 1 n, 0, ID ID, Item_Description Item_Desc, CAST(1.00 as double) QTY, UOM, 0.00 Scrap, Price Price From Materials
     Union All
-    select 1+ n, b.Child ID, m.Item_Description Item_Desc, b.Qty*r.Qty Qty, m.UOM, b.Scrap, m.Price From Bill_Of_Materials b
+    select 1 + n, b.Finished_Good, b.Child ID, m.Item_Description Item_Desc, b.Qty*r.Qty Qty, m.UOM, b.Scrap, m.Price From Bill_Of_Materials b
     inner join Materials m on b.Child = m.ID
     inner join receipt r on r.ID = b.Finished_Good
 )
-select * from receipt;
+select r.ID ID, m.Item_Description Item_Description, r.Qty*s.Qty Required from supply_orders s left join receipt r on s.Material = r.FG inner join Materials m on r.ID = m.ID where s.`Status` = "PRODUCTION";
