08bf36cf0bdb4399552d1102e561146b05346556
timofeyh <timofeyhartanovich@gmail.com>
2021-11-26 09:09:23 -0500

    updated item_receipt to have all items

--- item_receipt.sql
+++ item_receipt.sql
@@ -1,10 +1,13 @@
 use databasics;
-with recursive receipt(n, FG, ID, Item_Description, Qty, UOM, Scrap, Price) as 
+with recursive receipt(n, FG, FG_txt, Parent, Parent_txt, Child, Child_txt, Qty, UOM, Scrap, Price,`Group`) as 
 (
-	select 1 n, 0, ID ID, Item_Description Item_Desc, CAST(1.00 as double) QTY, UOM, 0.00 Scrap, Price Price From Materials
+	select 1 n, m.ID, m.Item_Description, m.ID, m.Item_Description, m2.ID Child, m2.Item_Description, CAST(1.00 as double) QTY, m2.UOM, 0.00 Scrap, m2.Price Price, m2.Group_ID From bill_of_materials b
+    inner join Materials m on b.Finished_Good = m.ID
+    inner join Materials m2 on b.Child = m2.ID
     Union All
-    select 1 + n, b.Finished_Good, b.Child ID, m.Item_Description Item_Desc, b.Qty*r.Qty Qty, m.UOM, b.Scrap, m.Price From Bill_Of_Materials b
-    inner join Materials m on b.Child = m.ID
-    inner join receipt r on r.ID = b.Finished_Good
+    select 1 + n, r.FG, r.FG_Txt, m.ID, m.Item_Description, m2.ID, m2.Item_Description, b.Qty*r.Qty, m2.UOM, b.Scrap, m2.Price, m2.Group_ID From Bill_Of_Materials b
+    inner join Materials m on b.Finished_Good = m.ID
+    inner join Materials m2 on b.Child = m2.ID
+    inner join receipt r on r.Child = m.ID
 )
-select s.ID `Order`, r.FG `Material`, m2.Item_Description `Produced Item`, r.ID ID, m.Item_Description `Consumed Item`, r.Qty*s.Qty Required from supply_orders s left join receipt r on s.Material = r.FG inner join Materials m on r.ID = m.ID inner join Materials m2 on r.FG = m2.ID where s.`Status` = "PRODUCTION";
+select * from receipt where FG = 9;
